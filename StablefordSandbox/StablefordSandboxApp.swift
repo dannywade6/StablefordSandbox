6ce05adcde7be396746144db82b69c58fa2fee87
Danny Wade <danny.lee.wade@gmail.com>
2022-06-18 12:16:49 +0100

    Initial Commit

---
+++ StablefordSandbox/StablefordSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  StablefordSandboxApp.swift
+//  StablefordSandbox
+//
+//  Created by Danny Wade on 18/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct StablefordSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
